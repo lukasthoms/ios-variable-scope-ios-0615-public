b09fdad578aaafcc85f55d0c726224d4ea52be1a
lukasthoms <luk@stho.ms>
2015-05-28 15:34:24 -0400

    finished

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -15,6 +15,26 @@
 
 @implementation FISAppDelegate
 
+-(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array {
+    NSMutableArray *myArray = [array mutableCopy];
+    [myArray addObject:string];
+    return myArray;
+}
+
+-(NSUInteger)countOfStringsInAllCapsInArray:(NSArray *)array {
+    NSUInteger matchCount = 0;
+    for (NSString *strings in array) {
+        if ([strings isEqualToString:[strings uppercaseString]]) {
+            matchCount = matchCount + 1;
+        }
+    }
+    return matchCount;
+}
+
+-(void)removeAllElementsFromArray:(NSMutableArray *)array {
+    [array removeAllObjects];
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
 
